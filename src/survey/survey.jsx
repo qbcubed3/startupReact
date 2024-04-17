9e52445fb79b2de6f01d35e06b12fa5b17a81e15
Ben Christensen <benjaminb.christensen@outlook.com>
2024-04-17 17:36:45 -0600

    SUrvey motly working

--- src/survey/survey.jsx
+++ src/survey/survey.jsx
@@ -4,24 +4,54 @@ export function Survey(){
     const[newItem, setNew] = useState('');
     const[deleteThing, setDelete] = useState('');
     
-    useEffect(() => {getItems();}, []);
-    
+    useEffect(() => {
+        getItems();
+      }, []);
+
     async function getItems(){
         const body2 = {
             authToken: localStorage.getItem("auth")
         }
         try{
-            response = await fetch('/api/survey/get', {
+            const response = await fetch('/api/survey/get', {
                 method: 'POST',
                 headers: {'content-type': 'application/json'},
                 body: JSON.stringify(body2)
             });
+            const data = await response.json();
+            if (data === null){
+                setItems([
+                "Morning meditation",
+                "Worked out",
+                "Ate Breakfast",
+                "Talked to a Friend",
+                "Learned something new",
+                "Took a walk",
+                "Listened to music",
+                "Did a Hobby",
+                "Read a book",
+                "Wrote in a journal",
+                "Ate lunch",
+                "Took Breaks from Work",
+                "Disconnect from technology for a bit",
+                "Had coffee/tea",
+                "Did something creative",
+                "Help someone in need",
+                "Planned for future goals",
+                "Laughed or watched something funny",
+                "Attend a social event",
+                "Ate Dinner",
+                "Had restful sleep",
+                "Unplugged before bedtime"
+              ]);
+            }
+            else{
+                setItems(data.items);
+            }
         }
         catch (error){
             console.log(error.message);
         }
-        const data = await response.json();
-        setItems(data.items);
     }
 
     async function addItem(){
@@ -59,7 +89,16 @@ export function Survey(){
             return;
         }
         setDelete('');
+    }
 
+    async function handleAdd(){
+        await addItem();
+        await getItems();
+    }
+
+    async function handleDelete(){
+        await deleteItem();
+        await getItems();
     }
 
     return(
@@ -67,14 +106,15 @@ export function Survey(){
             <h2>Survey</h2>
             <h4>What did you do today?</h4>
             <div id="checkboxes">
+                {items.map((item) => (<div><input type="checkbox" id={item}/><label>{item}</label></div>))}
             </div>
             <div>
                 <input type="text" id="newItem" placeholder="Add a New Activity" onChange={(e) => setNew(e.target.value)}></input>
-                <button class="my-button" id="addNew">Add</button>
+                <button class="my-button" id="addNew" onClick={handleAdd}>Add</button>
             </div>
             <div>
                 <input type="text" id="deleteItem" onChange={(e) => setDelete(e.target.value)} placeholder="Delete Activity"></input>
-                <button class="my-button" id="remove">Delete</button>
+                <button class="my-button" id="remove" onClick={handleDelete}>Delete</button>
             </div>
             <div class="ranges">
                 <h4>On a Scale of 1-10, How Happy were you Today?</h4>
