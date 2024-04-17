cac1cee362ada0111fa8979d17ec70153233099d
Ben Christensen <benjaminb.christensen@outlook.com>
2024-04-17 16:55:40 -0600

    survey begin

--- src/survey/survey.jsx
+++ src/survey/survey.jsx
@@ -1,11 +1,32 @@
-
+import React, {useState} from 'react'
 export function Survey(){
+    const[items, setItems] = useState([]);
+    const[newItem, setNew] = useState([]);
+    const[deleteItem, setDelete] = useState([]);
+
+
     return(
-        <div>
-            <div>
-                <p>Yo we made it to the survey  page</p>
+        <div class="survey">
+            <h2>Survey</h2>
+            <h4>What did you do today?</h4>
+            <div id="checkboxes">
             </div>
+            <div>
+                <input type="text" id="newItem" placeholder="Add a New Activity" onChange={(e) => setNew(e.target.value)}></input>
+                <button class="my-button" id="addNew">Add</button>
+            </div>
+            <div>
+                <input type="text" id="deleteItem" onChange={(e) => setDelete(e.target.value)} placeholder="Delete Activity"></input>
+                <button class="my-button" id="remove">Delete</button>
+            </div>
+            <div class="ranges">
+                <h4>On a Scale of 1-10, How Happy were you Today?</h4>
+                <input type="range" id = "happinessRange" name="happiness" min="1" max="10" step="1" value="5"></input>
+          <output id="selectedHappiness">5</output>
+          <br></br><br></br>
+          <button class="my-button" id="submit">Submit Survey</button>
         </div>
+      </div>
     )
 }
 
