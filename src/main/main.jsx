f5c7b62afd07f4cb3f6e96121ec9866a2081eb66
Ben Christensen <benjaminb.christensen@outlook.com>
2024-04-17 15:58:24 -0600

    login started

--- src/main/main.jsx
+++ src/main/main.jsx
@@ -1,12 +1,49 @@
+import React, { useState } from 'react';
 
 export function Main(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [code, setCode] = useState('');
+
+    const Login = async () => {
+        try{
+            const response = await fetch('/api/login',{
+                method: 'POST',
+                headers: {
+                  'content-type': 'application/json'
+                },
+                body: JSON.stringify({ username, password })
+              })
+            const data = await response.json();
+            const authToken = data.authToken;
+            localStorage.setItem("auth", authToken);
+            if (response.status !== 302){
+                setCode("You are logged in");
+            }
+            else{
+                setCode("Could not log you in. Try again.")
+            }
+        }
+        catch (error){
+            console.error(error);
+        }
+    }
+
     return(
-        <div>
-            <div>
-                <p>This is that big thing about how this app even works</p>
-            </div>
-        </div>
-    )
-}
+    <div className="login">
+      <h2>Login:</h2>
+      <label htmlFor="username">Username:</label>
+      <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter username"/>
+
+      <label htmlFor="password">Password:</label>
+      <input type="text" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password"/>
+
+      <button className="loginButton">
+        Login/Register
+      </button>
+      <p id="error">{code}</p>
+    </div>
+    );
+};
 
 export default Main;
